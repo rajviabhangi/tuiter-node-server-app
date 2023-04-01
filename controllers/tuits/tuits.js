09bda77b54be31c5de4fcf5719303dc7c450c848
Rajvi Abhangi <abhangirajvi@gmail.com>
2023-03-31 23:59:12 -0700

    Added data for unlikes

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -7,9 +7,11 @@ export default [
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
         "image": "../../images/spacex.jpg",
         "liked": true,
+        "unliked": false,
         "replies": 123,
         "retuits": 432,
         "likes": 2345,
+        "unlikes": 123,
         "handle": "@spacex",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
     },
@@ -23,9 +25,11 @@ export default [
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
         "image": "../../images/tesla.png",
         "liked": false,
+        "unliked": true,
         "replies": 234,
         "retuits": 543,
         "likes": 3456,
+        "unlikes": 500,
         "handle": "@tesla",
         "tuit": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
@@ -38,9 +42,11 @@ export default [
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
         "image": "../../images/spacex.jpg",
         "liked": true,
+        "unliked": false,
         "replies": 345,
         "retuits": 321,
         "likes": 1234,
+        "unlikes": 321,
         "handle": "@spacex",
         "tuit": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
     }
